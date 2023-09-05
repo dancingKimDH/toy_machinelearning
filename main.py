8180905c29622469a1ddb7c62addc1c22dee6c08
dancingkimDH <kimdonghyun0701@gmail.com>
2023-09-05 11:45:51 +0900

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # conda install -c conda-forge fastapi uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
+from fastapi import HTTPException
 app = FastAPI()
 
 # No 'Access-Control-Allow-Origin'
@@ -26,20 +26,40 @@ def mlmodelwithregression(data:dict) :
     print('data with dict {}'.format(data))
     # data dict to 변수 할당
     # !float으로 변수 datatype 지정해 주기!
-    texture_mean = float(data['texture_mean'])
-    perimeter_mean = float(data['perimeter_mean'])
+    
+    try:
+        hypertension = float(data['hypertension'])
+        gender = float(data['gender'])
+        liver_status = float(data['liver_status'])
+        age = float(data['age'])
+        weight = float(data['weight'])
+        surgery_duration = float(data['surgery_duration'])
 
-    # pkl 파일 존재 확인 코드 필요
 
-    result_predict = 0
-    # 학습 모델 불러와 예측
-    with open('datasets/BreastCancerWisconsin_Regression.pkl', 'rb') as regression_file:
-        loaded_model = pickle.load(regression_file)
-        input_labels = [[texture_mean, perimeter_mean]] # 학습했던 설명변수 형식 맞게 적용
-        result_predict = loaded_model.predict(input_labels)
-        print('Predict radius_mean Result : {}'.format(result_predict))
-        pass
+        import os
+        if not os.path.exists('datasets/BreastCancerWisconsin_Regression.pkl'):
+            raise HTTPException(status_code=500, detail='Model file not found')
 
-    # 예측값 리턴
-    result = {'radius_mean':result_predict[0]}
-    return result
+        # pkl 파일 존재 확인 코드 필요
+
+        result_predict = 0
+        # 학습 모델 불러와 예측
+        with open('datasets/BreastCancerWisconsin_Regression.pkl', 'rb') as regression_file:
+            loaded_model = pickle.load(regression_file)
+            input_labels = [[hypertension, gender, liver_status, age, weight, surgery_duration]] # 학습했던 설명변수 형식 맞게 적용
+            result_predict = loaded_model.predict(input_labels)
+            print('Predict radius_mean Result : {}'.format(result_predict))
+            pass
+
+        # 예측값 리턴
+        result = {'radius_mean':result_predict[0]}
+        return result
+
+    except KeyError as e:
+        raise HTTPException(status_code=400, detail=f'Missing or invalid data field: {e}')
+    
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=f'Invalid data type: {e}')
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'Internal server error: {e}')
