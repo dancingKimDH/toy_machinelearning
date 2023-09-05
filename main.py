db93a1492773ea4ef1154d7507d7b29d96ff38ad
dancingkimDH <kimdonghyun0701@gmail.com>
2023-09-05 12:56:29 +0900

    update

--- main.py
+++ main.py
@@ -28,26 +28,31 @@ def mlmodelwithregression(data:dict) :
     # !float으로 변수 datatype 지정해 주기!
     
     try:
-        hypertension = float(data['hypertension'])
-        gender = float(data['gender'])
-        liver_status = float(data['liver_status'])
-        age = float(data['age'])
+        hypertension_0 = int(data['hypertension_0'])
+        pass
+        hypertension_1 = int(data['hypertension_1'])
+        gender_1 = int(data['gender_1'])
+        gender_2 = int(data['gender_2'])
+        liver_status_0 = int(data['liver_status_0'])
+        liver_status_1 = int(data['liver_status_1'])
+        age = int(data['age'])
         weight = float(data['weight'])
         surgery_duration = float(data['surgery_duration'])
+        pass
 
+        # import os
+        # if not os.path.exists('datasets/BreastCancerWisconsin_Regression.pkl'):
+        #     raise HTTPException(status_code=500, detail='Model file not found')
 
-        import os
-        if not os.path.exists('datasets/BreastCancerWisconsin_Regression.pkl'):
-            raise HTTPException(status_code=500, detail='Model file not found')
-
-        # pkl 파일 존재 확인 코드 필요
+        # # pkl 파일 존재 확인 코드 필요
 
         result_predict = 0
         # 학습 모델 불러와 예측
-        with open('datasets/BreastCancerWisconsin_Regression.pkl', 'rb') as regression_file:
+        with open('datasets/RecurrenceOfSurgery_scaling.pkl', 'rb') as regression_file:
             loaded_model = pickle.load(regression_file)
-            input_labels = [[hypertension, gender, liver_status, age, weight, surgery_duration]] # 학습했던 설명변수 형식 맞게 적용
+            input_labels = [[hypertension_0, hypertension_1, gender_1, gender_2, liver_status_0, liver_status_1, age, weight, surgery_duration]] # 학습했던 설명변수 형식 맞게 적용
             result_predict = loaded_model.predict(input_labels)
+            pass
             print('Predict radius_mean Result : {}'.format(result_predict))
             pass
 
